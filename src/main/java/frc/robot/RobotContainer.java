c0ae0a1a85bfa411b171f545d6834d6550ae1bce
IcedJavaRobotics <68929234+Marsient@users.noreply.github.com>
2023-01-21 14:44:41 -0500

    in process, adding gyro
    meeting ended before a good stopping point, adding PID controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -36,7 +36,7 @@ public class RobotContainer {
     configureBindings(); {}
     
     driveTrainSubsystem.setDefaultCommand(
-      new RunCommand(() -> driveTrainSubsystem.mecanumDrive(-getJoystickX(), getJoystickY(), 0.87 * -getJoystickTwist(), flightStick.getThrottle(), flightStick.getRawButton(1)), driveTrainSubsystem)
+      new RunCommand(() -> driveTrainSubsystem.mecanumDrive(-getJoystickX(), getJoystickY(), 0.87 * -getJoystickTwist(), flightStick.getThrottle(), flightStick.getRawButton(1), flightStick.getRawButton(0)), driveTrainSubsystem)
     );
 
   }
--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -18,9 +19,17 @@ public class DriveTrainSubsystem extends SubsystemBase {
   final TalonFX backLeftTalon = new TalonFX(Constants.BACK_LEFT_TALON);
   final TalonFX frontRightTalon = new TalonFX(Constants.FRONT_RIGHT_TALON);
   final TalonFX backRightTalon = new TalonFX(Constants.BACK_RIGHT_TALON);
+
   double driveTime;
   double speedMod;
   double rampUpTime = 1.5;
+  double kP = 0;
+  double kI = 0;
+  double kD = 0;
+  int gyroPort;
+
+  private final AHRS ahrs = new AHRS(gyroPort);
+  public final PIDController balanceController = new PIDController(kP, kI, kD); 
 
   public DriveTrainSubsystem() {
 
@@ -43,36 +52,44 @@ public class DriveTrainSubsystem extends SubsystemBase {
 
   }
 
-  public void mecanumDrive( double X, double Y, double R, double Z, boolean zoom ) {
+  public void mecanumDrive( double X, double Y, double R, double Z, boolean zoom, boolean balance ) {
 
-    if (zoom == true) {     //When speed button is pressed it shortens ramp up time and puts it at max speed
-      Z = 1;
-      rampUpTime = 1;
-    } else {                //Normal ramp up time, speed dependant on the slider (Z)
-      Z = ( -Z + 1 )/2;
-      rampUpTime = 1.5;
-    }
+    if (balance == false) {
 
-    if ( Math.abs(X) + Math.abs(Y) + Math.abs(R) == 0 ) {
 
-      driveTime = Timer.getMatchTime();
-    
-    }
-
-    if ( Timer.getMatchTime() - driveTime <= rampUpTime ) {
-
-      speedMod = -1 * ((0.5 * (driveTime - Timer.getMatchTime()) / rampUpTime) + 0.5) ;
 
     } else {
-
-      speedMod = 1;
+      
+      if (zoom == true) {     //When speed button is pressed it shortens ramp up time and puts it at max speed
+        Z = 1;
+        rampUpTime = 1;
+      } else {                //Normal ramp up time, speed dependant on the slider (Z)
+        Z = ( -Z + 1 ) /2;
+        rampUpTime = 1.5;
+      }
+  
+      if ( Math.abs(X) + Math.abs(Y) + Math.abs(R) == 0 ) {
+  
+        driveTime = Timer.getMatchTime();
+      
+      }
+  
+      if ( Timer.getMatchTime() - driveTime <= rampUpTime ) {
+  
+        speedMod = -1 * ((0.5 * (driveTime - Timer.getMatchTime()) / rampUpTime) + 0.5) ;
+  
+      } else {
+  
+        speedMod = 1;
+  
+      }
+  
+      moveMotor( Z * speedMod * ensureRange(Y + X + R), frontLeftTalon);
+      moveMotor( Z * speedMod * ensureRange(Y - X + R), backLeftTalon);
+      moveMotor( Z * speedMod * ensureRange(Y - X - R), frontRightTalon);
+      moveMotor( Z * speedMod * ensureRange(Y + X - R), backRightTalon);
 
     }
-
-    moveMotor( Z * speedMod * ensureRange(Y + X + R), frontLeftTalon);
-    moveMotor( Z * speedMod * ensureRange(Y - X + R), backLeftTalon);
-    moveMotor( Z * speedMod * ensureRange(Y - X - R), frontRightTalon);
-    moveMotor( Z * speedMod * ensureRange(Y + X - R), backRightTalon);
 
   }
 
