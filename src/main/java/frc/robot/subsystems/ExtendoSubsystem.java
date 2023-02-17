0f7ea32f64dde16b01b50870002b8b2e1c2d0df9
IcedJavaRobotics <68929234+Marsient@users.noreply.github.com>
2023-02-16 19:05:10 -0500

    added PID to Extendo Subsystem
    added PID-controlled methods to Extendo Subsystem, added related constants

--- src/main/java/frc/robot/subsystems/ExtendoSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExtendoSubsystem.java
@@ -9,6 +9,7 @@ import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -16,8 +17,13 @@ import frc.robot.Constants;
 public class ExtendoSubsystem extends SubsystemBase {
   /** Creates a new ExtendoSubsystem. */
 
-  private TalonSRX extendoMotor = new TalonSRX(Constants.EXTENDO_MOTOR);
-  DigitalInput extendoLimitSwitch = new DigitalInput(Constants.EXTENDO_LIMIT_SWITCH);
+  private double kP = 0;                                                                // PID values
+  private double kI = 0;
+  private double kD = 0;
+
+  private TalonSRX extendoMotor = new TalonSRX(Constants.EXTENDO_MOTOR);                //motor
+  DigitalInput extendoLimitSwitch = new DigitalInput(Constants.EXTENDO_LIMIT_SWITCH);   //limit switch
+  PIDController extendoController = new PIDController(kP, kI, kD);                      //PID controller
 
   public ExtendoSubsystem() {
 
@@ -25,79 +31,6 @@ public class ExtendoSubsystem extends SubsystemBase {
     extendoMotor.setInverted(InvertType.None);
 
     extendoMotor.setNeutralMode(NeutralMode.Brake);
-
-  }
-
-  public Boolean extendoLower() {
-
-    if (extendoMotor.getSelectedSensorPosition() <= Constants.LOW_BOUND_LOW_POS) {
-
-      extendoExtend();
-      return true;
-    } else if (extendoMotor.getSelectedSensorPosition() >= Constants.HIGH_BOUND_LOW_POS) {
-
-      extendoRetract();
-      return true;
-    } else {
-
-      extendoStop();
-      return false;
-    }
-
-  }
-
-  public Boolean extendoMiddle() {
-
-    if (extendoMotor.getSelectedSensorPosition() <= Constants.LOW_BOUND_MID_POS) {
-
-      extendoExtend();
-      return true;
-    } else if (extendoMotor.getSelectedSensorPosition() >= Constants.HIGH_BOUND_MID_POS) {
-
-      extendoRetract();
-      return true;
-    } else {
-
-      extendoStop();
-      return false;
-    }
-    
-  }
-
-  public boolean extendoUpper() {
-
-    if (extendoMotor.getSelectedSensorPosition() <= Constants.LOW_BOUND_HIGH_POS) {
-
-      extendoExtend();
-      return true;
-
-    } else if (extendoMotor.getSelectedSensorPosition() >= Constants.HIGH_BOUND_HIGH_POS) {
-
-      extendoRetract();
-      return true;
-
-    } else {
-
-      extendoStop();
-
-    }
-    return false;
-  }
-
-  public void extendoDefault() {
-
-    if (extendoMotor.getSelectedSensorPosition() <= Constants.LOW_BOUND_DEFAULT_POS) {
-
-      extendoExtend();
-
-    } else if (extendoMotor.getSelectedSensorPosition() >= Constants.HIGH_BOUND_DEFAULT_POS) {
-
-      extendoRetract();
-
-    } else {
-
-      extendoStop();
-    }
 
   }
 
@@ -116,6 +49,78 @@ public class ExtendoSubsystem extends SubsystemBase {
   public void extendoStop() {
 
     extendoMotor.set(ControlMode.PercentOutput, 0);
+
+  }
+
+  public Boolean extendoLower() {             //extend to the lower goal method
+
+    extendoMotor.set(ControlMode.PercentOutput, extendoController.calculate(extendoMotor.getSelectedSensorPosition(), Constants.LOW_SETPOINT));  
+    
+    //I don't know if this will go up AND down or just one of the two
+
+
+    // if (encoder.getDistance() > Constants.LOW_BOUND_SETPOINT) {       //if the current distance is more than the setpoint
+
+    //   extendoMotor.set(ControlMode.PercentOutput, extendoController.calculate(encoder.getDistance(), Constants.LOW_BOUND_SETPOINT));
+
+    // }
+
+
+    if (extendoController.atSetpoint()) {     //if it's at the setpoint return false, if it isn't return true
+      return false;
+    } else {
+      return true;
+    }
+
+    // if (extendoMotor.getSelectedSensorPosition() <= Constants.LOW_BOUND_LOW_POS) {
+
+    //   extendoExtend();
+    //   return true;
+    // } else if (extendoMotor.getSelectedSensorPosition() >= Constants.HIGH_BOUND_LOW_POS) {
+
+    //   extendoRetract();
+    //   return true;
+    // } else {
+
+    //   extendoStop();
+    //   return false;
+    // }
+
+  }
+
+  public Boolean extendoMiddle() {
+
+    extendoMotor.set(ControlMode.PercentOutput, extendoController.calculate(extendoMotor.getSelectedSensorPosition(), Constants.MID_SETPOINT));  
+    
+    if (extendoController.atSetpoint()) {     //if it's at the setpoint return false, if it isn't return true
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+  public boolean extendoUpper() {
+
+    extendoMotor.set(ControlMode.PercentOutput, extendoController.calculate(extendoMotor.getSelectedSensorPosition(), Constants.HIGH_SETPOINT));  
+
+    if (extendoController.atSetpoint()) {     //if it's at the setpoint return false, if it isn't return true
+      return false;
+    } else {
+      return true;
+    }
+
+  }
+
+  public boolean extendoDefault() {
+
+    extendoMotor.set(ControlMode.PercentOutput, extendoController.calculate(extendoMotor.getSelectedSensorPosition(), Constants.DEFAULT_SETPOINT));  
+
+    if (extendoController.atSetpoint()) {     //if it's at the setpoint return false, if it isn't return true
+      return false;
+    } else {
+      return true;
+    }
 
   }
 
