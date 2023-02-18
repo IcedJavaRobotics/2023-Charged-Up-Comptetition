4d55620d68ba8ba67c65a0e8644ac7acc099e719
IcedJavaRobotics <57970708+IcedJavaRobotics@users.noreply.github.com>
2023-02-18 12:09:40 -0500

    Drivetrain motor number changed
    Motors numbers were changed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,10 +15,10 @@ package frc.robot;
 public final class Constants {
 
   //Drive train
-  public static final int FRONT_LEFT_TALON = 1;
-  public static final int FRONT_RIGHT_TALON = 2;
+  public static final int FRONT_LEFT_TALON = 4;
+  public static final int FRONT_RIGHT_TALON = 1;
   public static final int BACK_LEFT_TALON = 3;
-  public static final int BACK_RIGHT_TALON = 4;
+  public static final int BACK_RIGHT_TALON = 2;
 
   //Controllers
   public static final int JOYSTICK = 0;
